cf3208b25fc828d1fac5849fab11b2ba280270f6
Soh <example@gmail.com>
2023-06-14 14:24:54 +0300

    web programming"

--- Project1/030 praktika/js/script.js
+++ Project1/030 praktika/js/script.js
@@ -24,3 +24,29 @@ const movieDB = {
     ]
 };
 
+
+
+const adv = document.querySelectorAll('.promo__adv img'),
+    poster = document.querySelector('.promo__bg'),
+    genre = poster.querySelector('.promo__genre'),
+    movieList = document.querySelector('.promo__interactive-list');
+
+
+
+
+adv.forEach (item =>{
+        item.remove();
+});// Задание 1
+
+genre.textContent = "драма";//Задание 2
+poster.style.backgroundImage = 'url("img/bg.jpg")';//Задание 3
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i)=>{
+    movieList.innerHTML += `<li class="promo__interactive-item">${i + 1} ${film}
+    <div class="delete"></div>
+</li>`;
+});
